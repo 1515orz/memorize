1d87c983555276ccefe16f611d83b695bf4436c3
1515orz <bearnwy@126.com>
2024-04-19 15:29:31 +0800

    test

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,21 +8,43 @@
 import SwiftUI
 import SwiftData
 
-//test
 
 struct ContentView: View {
     //    @Query private var items: [Item]
-    let emojis = ["🥲", "😂", "😎", "🥵","😍"]
+    let emojis = ["🥲", "😂", "😎", "🥵","😍", "🥲", "😂", "😎", "🥵","😍"]
+    @State var cardCount = 4
     
     var body: some View {
-        
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
+        VStack{
+            HStack {
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content: emojis[index])
+                }
             }
+            .foregroundColor(.orange)
+            .padding()
+            HStack {
+                Button(action: {
+                    if cardCount > 1 {
+                        cardCount -= 1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.badge.minus.fill")
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    if cardCount < emojis.count {
+                        cardCount += 1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.fill.badge.plus")
+                })
+            }
+            .padding()
+            .imageScale(.large)
         }
-        .foregroundColor(.orange)
-        .padding()
     }
 }
 
